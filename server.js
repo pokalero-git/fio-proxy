1c73b6a962ddc06d615e57542458811601bb6fe0
pokalero-git <michalpokalero@gmail.com>
2025-10-21 17:56:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,4 +17,5 @@ app.get("/fio", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Proxy běží na portu ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Proxy běží na portu ${PORT}`));
+
